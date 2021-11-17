b45a7d469e40cbd7be7f7f3579b6705bd4d55c0e
Balram <webdevforindia@gmail.com>
2021-11-17 16:49:49 +0530

    1.1.1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var multerS3 = require("multer-s3");
 var app = express();
 var cors = require("cors");
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'https://iskcon-solapur.web.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
