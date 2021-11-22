854df0248e6d269f88409feabdcd1b164f55dec6
Balram <webdevforindia@gmail.com>
2021-11-22 16:13:44 +0530

    1.1.2

--- Controllers/getControllers.js
+++ Controllers/getControllers.js
@@ -2,7 +2,6 @@ const express = require("express");
 const Router = express.Router();
 const s3_Connection = require("../Connections/AWS_Connections");
 
-
 // Router.get("/getimagesurl", (req, res) => {
 //   s3_Connection.listObjects({ Bucket: "bhaktivedant-bucketv" }, (err, data) => {
 //     if (err) {
@@ -19,20 +18,57 @@ const s3_Connection = require("../Connections/AWS_Connections");
 //   });
 // });
 
-Router.get("/getimagesurl/:date", (req,res) => {
+Router.get("/getimagesurl/:date", (req, res) => {
   const date = req.params.date;
-  s3_Connection.listObjects({ Bucket: "bhaktivedant-bucketv"}, (err,data) => {
+  s3_Connection.listObjects({ Bucket: "bhaktivedant-bucketv" }, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const imageData = data.Contents.map((img) => {
+        return {
+          key: img.Key ? (img.Key.startsWith(date) && img.Key) : null,
+        };
+      });
+      var myFilterArray = imageData.filter((img) => {
+        if (img.key){
+          return img
+        }
+      });
+     res.send(myFilterArray);
+    }
+  });
+});
+
+Router.get("/getfoldernames", (req, res) => {
+  s3_Connection.listObjects({ Bucket: "bhaktivedant-bucketv" }, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const imageData = data.Contents.map((img) => {
+        return {
+          key: img.Key ? (img.Key.split("/").length >= 2 && img.Key.split("/")[1] != "" ? img.Key : null) : null,
+        };
+      });
+      res.send(imageData);
+    }
+  });
+});
+
+Router.get("/folder/:foldername",(req,res) => {
+  const foldername = req.params.foldername;
+  s3_Connection.listObjects({ Bucket: "bhaktivedant-bucketv", Prefix: req.params.foldername }, (err, data) => {
     if (err){
       console.log(err)
     }
     else{
       const imageData = data.Contents.map((img) => {
         return {
-            key : img.Key ? img.Key.startsWith(date) ? img.Key : null : null,
-        }
+          key: img.Key ? (img.Key.startsWith(foldername) && img.Key.split("/")[1] != "" ? img.Key : null) : null,
+        };
       })
       res.send(imageData);
     }
   })
 })
+
 module.exports = Router;
