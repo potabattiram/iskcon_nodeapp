886ca4f582c527be15417128dd4aa7bca54f29ec
Balram <webdevforindia@gmail.com>
2021-12-02 14:51:48 +0530

    1.2.3

--- Connections/AWS_Connections.js
+++ Connections/AWS_Connections.js
@@ -1,8 +1,8 @@
 const aws = require("aws-sdk");
 
 const S3_Connection = new aws.S3({
-  accessKeyId: "AKIA6PX5RHJWIYF3LAVN",
-  secretAccessKey: "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",
+  accessKeyId: "AKIA6PX5RHJWD3FDPEPG",
+  secretAccessKey: "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",
   Bucket: "bhaktivedant-bucketv",
 });
 
