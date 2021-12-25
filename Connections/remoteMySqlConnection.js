655f04926ae5052a071344c34e79abd7a53bcf39
Balram <webdevforindia@gmail.com>
2021-12-25 16:06:03 +0530

    1.1.2

--- Connections/remoteMySqlConnection.js
+++ Connections/remoteMySqlConnection.js
@@ -1,21 +1,21 @@
-// var mysql = require('mysql');
-// var pool  = mysql.createPool({
-//   connectionLimit: 20,
-//   host: "remotemysql.com",
-//   port: 3306,
-//   user: "vLGAtpQiCG",
-//   password: "y8blMNVpZ6",
-//   database: "vLGAtpQiCG",
-//   port: 3306,
-// });
+var mysql = require('mysql');
+var pool  = mysql.createPool({
+  connectionLimit: 20,
+  host: "remotemysql.com",
+  port: 3306,
+  user: "vLGAtpQiCG",
+  password: "y8blMNVpZ6",
+  database: "vLGAtpQiCG",
+  port: 3306,
+});
 
-// pool.getConnection(function(err, connection) {
-//   connection.query('SELECT * FROM Towels_Data limit 1', function (error, results, fields) {
-//     connection.release();
-//     if (error) throw error;
-//     else{
-//       console.log("Connected & Data Available")
-//     }
-//   });
-// });
-// module.exports = pool;
+pool.getConnection(function(err, connection) {
+  connection.query('SELECT * FROM Towels_Data limit 1', function (error, results, fields) {
+    connection.release();
+    if (error) throw error;
+    else{
+      console.log("Connected & Data Available")
+    }
+  });
+});
+module.exports = pool;
